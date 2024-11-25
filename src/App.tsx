ccf715f0eb980826cc3b2feb56b8477dba3df0e9
Wutche <wuthchechikaome75@gmail.com>
2024-11-25 00:58:53 -0600

    added some few heroes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import './App.css'
+/** @format */
+
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
+} from "react-router-dom";
+import "./App.css";
+import UserLayout from "./layouts/UserLayout";
+import { Home } from "./pages";
 
 function App() {
+	const router = createBrowserRouter(
+		createRoutesFromElements(
+			<>
+				<Route path="/" element={<UserLayout />}>
+					<Route index element={<Home />} />
+				</Route>
+			</>,
+		),
+	);
 
-  return (
-    <>
-      <p>Hello World</p>
-    </>
-  )
+	return (
+		<div>
+			<RouterProvider router={router} />
+		</div>
+	);
 }
 
-export default App
+export default App;
